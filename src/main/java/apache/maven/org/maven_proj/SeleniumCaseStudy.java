06056cd69578d24a344af0281c3ba925ba6b5c33
Sanjay Kumar <sanjaysah.engg@gmail.com>
2018-05-20 16:52:38 +0530

    testng.xml added and entried in pom.xml too

---
+++ src/main/java/apache/maven/org/maven_proj/SeleniumCaseStudy.java
@@ -0,0 +1,37 @@
+package apache.maven.org.maven_proj;
+
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class SeleniumCaseStudy {
+	WebDriver driver;
+	@Test 
+	public void seleniumTest() { 
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Driver\\chromedriver.exe");
+		ChromeOptions chromeOptions = new ChromeOptions(); 
+		driver = new ChromeDriver(chromeOptions); 
+		//String url = "http://localhost:9090/addressbook";
+		driver.get("http://facebook.com");
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); 
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("sweet.sanjay@gmail.com"); 
+		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("Sonu0383"); 
+		driver.findElement(By.xpath("//*[@value='Log In']")).click();
+		boolean actual = driver.getTitle().contains("Facebook");
+		Assert.assertTrue(actual);
+		//Thread.sleep(5000); 
+		driver.quit(); 
+	}
+	@AfterMethod
+	public void after(){
+		driver.quit();
+	}
+}
